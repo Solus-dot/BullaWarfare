53d0a8bf7fcc0aaea783749158c1282b94263934
Solus-dot <solus23451@gmail.com>
2024-06-17 16:21:26 +0530

    edited gitignore

---
+++ Assets/Scripts/Movesets/Sarv.cs
@@ -0,0 +1,16 @@
+// Sarvesh
+
+// Move 1: Domi Mommy
+// As the dom mommy of the group, Sarvesh holds absolute authority over the opponent.
+// Asks the opponent to lower both their Attack and Defence by 1 and the opponent gladly accepts.
+
+// Move 2: Veteran Climber
+// Sarvesh uses his veteran climbing abilities and takes the high ground and rests,
+// Sarvesh recovers 75% of total HP but is unable to attack the next turn.
+
+// Move 3: WOOOOOW
+// Sarvesh gathers up air in his chest and with full volume screams WOOOOOOW into the opponent's ear. Deals damage, 20% flinch chance
+
+// Move 4: Churan Heatwave
+// Sarvesh ingests his special Churan passed onto him by his ancestors and releases a massive heatwave which damages the opponent. Deals damage and has a 50% chance to lower enemy's defence
+
