068e04f5f9202a8e7dbd04f981ea3a4d67ec61fd
Solus-dot <solus23451@gmail.com>
2024-06-18 13:33:02 +0530

    Added Icons for atk and def changes

--- Assets/Scripts/BattleHUD.cs
+++ Assets/Scripts/BattleHUD.cs
@@ -8,6 +8,15 @@
 	public TMP_Text nameText;			// Reference to the text displaying unit's name
 	public TMP_Text levelText;			// Reference to the text displaying unit's level
 	public TMP_Text buffText;			// Reference to the text displaying unit's current buff/debuff status
+
+	public SpriteRenderer AtkIcon;		// Reference to the icon used for attack stages
+	public SpriteRenderer DefIcon;		// Reference to the icon used for defense stages
+
+	public Sprite AttackUp;
+	public Sprite AttackDown;
+	public Sprite DefenseUp;
+	public Sprite DefenseDown;
+
 	public Slider hpSlider;				// Reference to the slider representing unit's HP
 	public Gradient hpGradient;			// Gradient for HP bar color
 
@@ -15,17 +24,44 @@
 	{
 		nameText.text = unit.unitName;				// Update the name text with unit's name
 		levelText.text = "Lvl " + unit.unitLevel;	// Update the level text with unit's level
-		buffText.text = "Atk+:" + unit.attackStage + "\nDef+:" + unit.defenseStage;			// Update the buff text with unit's buffs
 
-		hpSlider.maxValue = unit.maxHP;		// Set max value of HP slider to unit's max HP
-		hpSlider.value = unit.currentHP;	// Set current value of HP slider to unit's current HP
+		AtkIcon.gameObject.SetActive(false);
+		DefIcon.gameObject.SetActive(false);
+		buffText.text = "";							// Leave blank at the start
+
+		hpSlider.maxValue = unit.maxHP;				// Set max value of HP slider to unit's max HP
+		hpSlider.value = unit.currentHP;			// Set current value of HP slider to unit's current HP
 
 		// Update HP bar color based on HP percentage
 		UpdateHPColor();
 	}
 
 	public void SetStatChange(Unit unit) {
-		buffText.text = "Atk+:" + unit.attackStage + "\nDef+:" + unit.defenseStage;
+		buffText.text = "";
+		AtkIcon.gameObject.SetActive(false);
+		DefIcon.gameObject.SetActive(false);
+		
+		if(unit.attackStage != 0) {
+			AtkIcon.gameObject.SetActive(true);
+			if (unit.attackStage > 0) {
+				AtkIcon.sprite = AttackUp;
+				buffText.text += "\t" + unit.attackStage;
+			} else if (unit.attackStage < 0) {
+				AtkIcon.sprite = AttackDown;
+				buffText.text += "\t" + unit.attackStage;
+			}
+		}
+
+		if(unit.defenseStage != 0) {
+			DefIcon.gameObject.SetActive(true);
+			if (unit.defenseStage > 0) {
+				DefIcon.sprite = DefenseUp;
+				buffText.text += "\n\t" + unit.defenseStage;
+			} else if (unit.defenseStage < 0) {
+				DefIcon.sprite = DefenseDown;
+				buffText.text += "\n\t" + unit.defenseStage;
+			}
+		}
 	}
 
 	public void SetHP(int hp)
