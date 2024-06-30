84aa076fdb8d8d5e50d681747d92451da7cbd69e
Solus-dot <solus23451@gmail.com>
2024-06-30 20:48:09 +0530

    Added Volume across whole game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,40 +2,37 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
-{
+public class MainMenu : MonoBehaviour {
 	public Slider volumeSlider;
 
-	void Start()
-	{
-		if (volumeSlider != null)
-		{
+	void Start() {
+		if (volumeSlider != null) {
 			volumeSlider.onValueChanged.AddListener(delegate { SnapSliderValue(); });
+			volumeSlider.value = PlayerPrefs.GetFloat("Volume", 100f); // Initialize the value from PlayerPrefs
 			SnapSliderValue(); // Initialize the value to the nearest multiple of 10
 		}
 	}
 
-	void SnapSliderValue()
-	{
+	void SnapSliderValue() {
 		// Snap the slider value to the nearest multiple of 10
 		float snappedValue = Mathf.Round(volumeSlider.value / 10) * 10;
 		volumeSlider.value = snappedValue;
 
 		// Update the audio source volume
-		if (AudioManager.Instance != null)
-		{
+		if (AudioManager.Instance != null) {
 			AudioManager.Instance.SetVolume(snappedValue / 100f); // Assuming the slider range is from 0 to 100
 		}
+
+		// Save the volume setting
+        PlayerPrefs.SetFloat("Volume", volumeSlider.value);
 	}
 
-	public void OnPlayButtonClick()
-	{
+	public void OnPlayButtonClick() {
 		Debug.Log("Start Game button clicked");
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 
-	public void OnExitButtonClick()
-	{
+	public void OnExitButtonClick() {
 		Debug.Log("Exit button clicked");
 		Application.Quit();
 	}
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class PauseMenu : MonoBehaviour {
 	public GameObject tintPanel;
 	public GameObject pauseMenuPanel;
+	public Slider volumeSlider;
 
 	private bool isPaused;
 
@@ -12,6 +13,14 @@ public class PauseMenu : MonoBehaviour {
 		isPaused = false;
 		tintPanel.SetActive(false);
 		pauseMenuPanel.SetActive(false);
+
+		if (volumeSlider != null)
+        {
+            volumeSlider.onValueChanged.AddListener(delegate { SnapSliderValue(); });
+            volumeSlider.value = PlayerPrefs.GetFloat("Volume", 50f); // Initialize the value from PlayerPrefs
+            SnapSliderValue(); // Initialize the value to the nearest multiple of 10
+        }
+
 	}
 
 	void Update() {
@@ -54,5 +63,19 @@ public class PauseMenu : MonoBehaviour {
 		}
 
 		Resume();
+	}
+
+	void SnapSliderValue() {
+		// Snap the slider value to the nearest multiple of 10
+		float snappedValue = Mathf.Round(volumeSlider.value / 10) * 10;
+		volumeSlider.value = snappedValue;
+
+		// Update the audio source volume
+		if (AudioManager.Instance != null) {
+			AudioManager.Instance.SetVolume(snappedValue / 100f); // Assuming the slider range is from 0 to 100
+		}
+
+		// Save the volume setting
+		PlayerPrefs.SetFloat("Volume", volumeSlider.value);
 	}
 }
