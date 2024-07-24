60c9b60fee176bfb6675a13b54677f319a4cbcca
Solus-dot <solus23451@gmail.com>
2024-07-24 19:38:41 +0530

    Added battle flow like PokemonShowdown

--- Assets/Scripts/Network/MPBattleSystem.cs
+++ Assets/Scripts/Network/MPBattleSystem.cs
@@ -25,6 +25,10 @@ public class MPBattleSystem : NetworkBehaviour {
 	[SerializeField] private Button moveButton3;
 	[SerializeField] private Button moveButton4;
 
+	[Header("Dialogue")]
+	[SerializeField] private TMP_Text dialogueText;
+	[SerializeField] private TMP_Text moveText;
+
 	private TMP_Text moveButton1Text;
 	private TMP_Text moveButton2Text;
 	private TMP_Text moveButton3Text;
@@ -33,7 +37,11 @@ public class MPBattleSystem : NetworkBehaviour {
 	private int? hostSelectedMove = null;
 	private int? clientSelectedMove = null;
 
+	private string playerName;
+
 	private void Start() {
+		playerName = PlayerPrefs.GetString("Name", "Player");
+
 		moveButton1Text = moveButton1.GetComponentInChildren<TMP_Text>();
 		moveButton2Text = moveButton2.GetComponentInChildren<TMP_Text>();
 		moveButton3Text = moveButton3.GetComponentInChildren<TMP_Text>();
@@ -50,6 +58,166 @@ public class MPBattleSystem : NetworkBehaviour {
 		} else {
 			Debug.Log("Client is spawning character: " + SelectedCharacterData.ClientCharacterName);
 			SpawnCharacterServerRpc(SelectedCharacterData.ClientCharacterName, false);
+		}
+
+		SetInitialUIState();
+	}
+
+	private void SetInitialUIState() {
+		dialogueText.text = $"{playerName}, pick your move.";
+		moveText.gameObject.SetActive(false);
+	}
+
+	private void OnMoveButtonClicked(int moveIndex) {
+		Debug.Log($"Move button {moveIndex + 1} clicked.");
+		if (IsHost) {
+			hostSelectedMove = moveIndex;
+			Debug.Log($"Host selected move {moveIndex}");
+			UpdateUIAfterMoveSelection(true);
+		} else {
+			clientSelectedMove = moveIndex;
+			Debug.Log($"Client selected move {moveIndex}");
+			SelectMoveServerRpc(moveIndex);  // Inform server about client move
+			ShowWaitingForOpponentUI(); // Update client UI immediately
+		}
+		CheckMovesAndExecute(); // Check moves immediately after selection
+	}
+
+	private void UpdateUIAfterMoveSelection(bool isHost) {
+		if (isHost) {
+			if (!clientSelectedMove.HasValue) {
+				ShowWaitingForOpponentUI();
+			}
+		} else {
+			if (!hostSelectedMove.HasValue) {
+				ShowWaitingForOpponentUI();
+			}
+		}
+	}
+
+	private void ShowWaitingForOpponentUI() {
+		dialogueText.gameObject.SetActive(false);
+		moveButton1.gameObject.SetActive(false);
+		moveButton2.gameObject.SetActive(false);
+		moveButton3.gameObject.SetActive(false);
+		moveButton4.gameObject.SetActive(false);
+
+		moveText.gameObject.SetActive(true);
+		moveText.text = "Waiting for opponent...";
+	}
+
+	[ServerRpc(RequireOwnership = false)]
+	private void SelectMoveServerRpc(int clientMove, ServerRpcParams rpcParams = default) {
+		clientSelectedMove = clientMove;
+		Debug.Log($"Server registered client move: {clientMove}");
+		UpdateHostUIAfterClientMoveSelection(); // Ensure the host UI updates correctly
+		CheckMovesAndExecute();
+	}
+
+	private void UpdateHostUIAfterClientMoveSelection() {
+		if (hostSelectedMove.HasValue) {
+			ShowWaitingForOpponentUI();
+		}
+	}
+
+	private void CheckMovesAndExecute() {
+		if (hostSelectedMove.HasValue && clientSelectedMove.HasValue) {
+			Debug.Log("Both moves selected, executing moves.");
+			ExecuteMovesServerRpc(hostSelectedMove.Value, clientSelectedMove.Value);
+			hostSelectedMove = null;
+			clientSelectedMove = null;
+		}
+	}
+
+	private void ResetUIAfterMoveExecution() {
+		moveText.gameObject.SetActive(false);
+		dialogueText.gameObject.SetActive(true);
+		moveButton1.gameObject.SetActive(true);
+		moveButton2.gameObject.SetActive(true);
+		moveButton3.gameObject.SetActive(true);
+		moveButton4.gameObject.SetActive(true);
+
+		dialogueText.text = $"{playerName}, pick your move.";
+	}
+
+	[ServerRpc(RequireOwnership = false)]
+	private void ExecuteMovesServerRpc(int hostMove, int clientMove) {
+		Debug.Log($"Executing moves: HostMove = {hostMove}, ClientMove = {clientMove}");
+		List<System.Action> moveActions = new List<System.Action> {
+			() => PerformMove(hostUnit, clientUnit, hostMove, true),
+			() => PerformMove(clientUnit, hostUnit, clientMove, false)
+		};
+
+		moveActions.Shuffle(); // Randomize move execution order
+
+		foreach (var moveAction in moveActions) {
+			moveAction();
+		}
+
+		// Inform clients to reset their UI after moves execution
+		ResetUIAfterMoveExecutionClientRpc();
+	}
+
+	[ClientRpc]
+	private void ResetUIAfterMoveExecutionClientRpc() {
+		ResetUIAfterMoveExecution();
+	}
+
+	private void PerformMove(Unit attacker, Unit defender, int moveIndex, bool isHost) {
+		Debug.Log($"{(isHost ? "Host" : "Client")} performing move {moveIndex} on {(isHost ? "Client" : "Host")}.");
+		// Get move and calculate damage
+		Move move = attacker.GetMove(moveIndex);
+		int damage = CalculateDamage(attacker, defender, move);
+
+		// Apply damage
+		defender.TakeDamage(damage);
+		UpdateHealthClientRpc(defender.currentHP, defender.maxHP, !isHost);
+
+		// Apply stat changes
+		attacker.TakeBuff(move.selfAttackChange, move.selfDefenseChange);
+		defender.TakeBuff(move.oppAttackChange, move.oppDefenseChange);
+
+		// Update stat changes on both host and client
+		UpdateStatChangesClientRpc(attacker.attackStage, attacker.defenseStage, isHost, attacker.name);
+		UpdateStatChangesClientRpc(defender.attackStage, defender.defenseStage, !isHost, defender.name);
+	}
+
+	private int CalculateDamage(Unit attacker, Unit defender, Move move) {
+		return (attacker.attack * move.damage) / defender.defense;
+	}
+
+	[ClientRpc]
+	private void UpdateHealthClientRpc(int currentHP, int maxHP, bool isHost) {
+		Debug.Log($"Updating health for {(isHost ? "Host" : "Client")} to {currentHP}/{maxHP}");
+		if (isHost) {
+			hostHUD.SetHP(currentHP);
+		} else {
+			clientHUD.SetHP(currentHP);
+		}
+	}
+
+	[ClientRpc]
+	private void UpdateStatChangesClientRpc(int attackStage, int defenseStage, bool isHost, string unitName) {
+		Debug.Log($"Updating stat changes for {unitName} - {(isHost ? "Host" : "Client")}. Attack Stage: {attackStage}, Defense Stage: {defenseStage}");
+
+		if (isHost) {
+			hostUnit.attackStage = attackStage;
+			hostUnit.defenseStage = defenseStage;
+			hostHUD.SetStatChange(hostUnit);
+		} else {
+			clientUnit.attackStage = attackStage;
+			clientUnit.defenseStage = defenseStage;
+			clientHUD.SetStatChange(clientUnit);
+		}
+	}
+
+	private void UpdateMoveButtons() {
+		Unit localPlayerUnit = IsHost ? hostUnit : clientUnit;
+		if (localPlayerUnit != null) {
+			moveButton1Text.text = localPlayerUnit.GetMove(0).moveName;
+			moveButton2Text.text = localPlayerUnit.GetMove(1).moveName;
+			moveButton3Text.text = localPlayerUnit.GetMove(2).moveName;
+			moveButton4Text.text = localPlayerUnit.GetMove(3).moveName;
 		}
 	}
 
@@ -119,109 +287,6 @@ public class MPBattleSystem : NetworkBehaviour {
 			}
 		}
 		return null;
-	}
-
-	private void OnMoveButtonClicked(int moveIndex) {
-		Debug.Log($"Move button {moveIndex + 1} clicked.");
-		if (IsHost) {
-			hostSelectedMove = moveIndex;
-			Debug.Log($"Host selected move {moveIndex}");
-		} else {
-			clientSelectedMove = moveIndex;
-			Debug.Log($"Client selected move {moveIndex}");
-			SelectMoveServerRpc(moveIndex);  // Inform server about client move
-		}
-
-		CheckMovesAndExecute();
-	}
-
-	[ServerRpc(RequireOwnership = false)]
-	private void SelectMoveServerRpc(int clientMove, ServerRpcParams rpcParams = default) {
-		clientSelectedMove = clientMove;
-		Debug.Log($"Server registered client move: {clientMove}");
-		CheckMovesAndExecute();
-	}
-
-	private void CheckMovesAndExecute() {
-		if (hostSelectedMove.HasValue && clientSelectedMove.HasValue) {
-			Debug.Log("Both moves selected, executing moves.");
-			ExecuteMovesServerRpc(hostSelectedMove.Value, clientSelectedMove.Value);
-			hostSelectedMove = null;
-			clientSelectedMove = null;
-		}
-	}
-
-	[ServerRpc(RequireOwnership = false)]
-	private void ExecuteMovesServerRpc(int hostMove, int clientMove) {
-		Debug.Log($"Executing moves: HostMove = {hostMove}, ClientMove = {clientMove}");
-		List<System.Action> moveActions = new List<System.Action> {
-			() => PerformMove(hostUnit, clientUnit, hostMove, true),
-			() => PerformMove(clientUnit, hostUnit, clientMove, false)
-		};
-
-		moveActions.Shuffle(); // Randomize move execution order
-
-		foreach (var moveAction in moveActions) {
-			moveAction();
-		}
-	}
-
-	private void PerformMove(Unit attacker, Unit defender, int moveIndex, bool isHost) {
-		Debug.Log($"{(isHost ? "Host" : "Client")} performing move {moveIndex} on {(isHost ? "Client" : "Host")}.");
-		// Get move and calculate damage
-		Move move = attacker.GetMove(moveIndex);
-		int damage = CalculateDamage(attacker, defender, move);
-
-		// Apply damage
-		defender.TakeDamage(damage);
-		UpdateHealthClientRpc(defender.currentHP, defender.maxHP, !isHost);
-
-		// Apply stat changes
-		attacker.TakeBuff(move.selfAttackChange, move.selfDefenseChange);
-		defender.TakeBuff(move.oppAttackChange, move.oppDefenseChange);
-
-		// Update stat changes on both host and client
-		UpdateStatChangesClientRpc(attacker.attackStage, attacker.defenseStage, isHost, attacker.name);
-		UpdateStatChangesClientRpc(defender.attackStage, defender.defenseStage, !isHost, defender.name);
-	}
-
-	private int CalculateDamage(Unit attacker, Unit defender, Move move) {
-		return (attacker.attack * move.damage) / defender.defense;
-	}
-
-	[ClientRpc]
-	private void UpdateHealthClientRpc(int currentHP, int maxHP, bool isHost) {
-		Debug.Log($"Updating health for {(isHost ? "Host" : "Client")} to {currentHP}/{maxHP}");
-		if (isHost) {
-			hostHUD.SetHP(currentHP);
-		} else {
-			clientHUD.SetHP(currentHP);
-		}
-	}
-
-	[ClientRpc]
-	private void UpdateStatChangesClientRpc(int attackStage, int defenseStage, bool isHost, string unitName) {
-		Debug.Log($"Updating stat changes for {unitName} - {(isHost ? "Host" : "Client")}. Attack Stage: {attackStage}, Defense Stage: {defenseStage}");
-
-		if (isHost) {
-			hostUnit.attackStage = attackStage;
-			hostUnit.defenseStage = defenseStage;
-			hostHUD.SetStatChange(hostUnit);
-		} else {
-			clientUnit.attackStage = attackStage;
-			clientUnit.defenseStage = defenseStage;
-			clientHUD.SetStatChange(clientUnit);
-		}
-	}
-
-	private void UpdateMoveButtons() {
-		Unit localPlayerUnit = IsHost ? hostUnit : clientUnit;
-		if (localPlayerUnit != null) {
-			moveButton1Text.text = localPlayerUnit.GetMove(0).moveName;
-			moveButton2Text.text = localPlayerUnit.GetMove(1).moveName;
-			moveButton3Text.text = localPlayerUnit.GetMove(2).moveName;
-			moveButton4Text.text = localPlayerUnit.GetMove(3).moveName;
-		}
 	}
 }
 
